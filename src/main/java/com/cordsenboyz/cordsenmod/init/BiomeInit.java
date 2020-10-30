2dce9d74a87085b875694e2a1c7d0d8e22604d98
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2020-10-30 12:33:03 +0100

    Daily Update

--- src/main/java/com/cordsenboyz/cordsenmod/init/BiomeInit.java
+++ src/main/java/com/cordsenboyz/cordsenmod/init/BiomeInit.java
@@ -44,11 +44,18 @@ public class BiomeInit {
                             category(Biome.Category.ICY).downfall(0.8F).depth(1.0F).parent(null)));
 
     public static final RegistryObject<Biome> LAVALANDS = BIOMES.register("lavalands",
-            () -> new LavaLands(new Biome.Builder().precipitation(Biome.RainType.NONE).scale(0.2F).temperature(10.0F)
+            () -> new LavaLands(new Biome.Builder().precipitation(Biome.RainType.NONE).scale(0.1F).temperature(10.0F)
                     .waterColor(4159204).waterFogColor(329011)
                     .surfaceBuilder(SurfaceBuilder.DEFAULT, new SurfaceBuilderConfig(BlockInit.BLACKSTONE.get().getDefaultState(),
                             BlockInit.BLACKSTONE.get().getDefaultState(), BlockInit.BLACKSTONE.get().getDefaultState())).
                             category(Biome.Category.PLAINS).downfall(0.0F).depth(0.1F).parent(null)));
+
+    public static final RegistryObject<Biome> REDDESERT = BIOMES.register("reddesert",
+            () -> new RedDesert(new Biome.Builder().precipitation(Biome.RainType.NONE).scale(0.05F).temperature(2.0F)
+            .waterColor(4159204).waterFogColor(329011)
+                    .surfaceBuilder(SurfaceBuilder.DEFAULT, new SurfaceBuilderConfig(Blocks.RED_SAND.getDefaultState(),
+                            Blocks.RED_SANDSTONE.getDefaultState(), Blocks.RED_SANDSTONE.getDefaultState())).
+                            category(Biome.Category.DESERT).downfall(0.0F).depth(0.125F).parent(null)));
 
     public static void registerBiomes() {
         registerBiome(ANCIENTMOUNTAINS.get(), BiomeDictionary.Type.HILLS, BiomeDictionary.Type.OVERWORLD);
@@ -56,6 +63,7 @@ public class BiomeInit {
         registerBiome(CHARREDFOREST.get(), BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.OVERWORLD);
         registerBiome(ICEMOUNTAINS.get(), BiomeDictionary.Type.COLD, BiomeDictionary.Type.OVERWORLD);
         registerBiome(LAVALANDS.get(), BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.OVERWORLD);
+        registerBiome(REDDESERT.get(), BiomeDictionary.Type.HOT, BiomeDictionary.Type.OVERWORLD);
     }
 
     private static void registerBiome(Biome biome, BiomeDictionary.Type... types){
--- src/main/java/com/cordsenboyz/cordsenmod/objects/items/HammerItem.java
+++ src/main/java/com/cordsenboyz/cordsenmod/objects/items/HammerItem.java
@@ -8,7 +8,7 @@ import net.minecraft.item.PickaxeItem;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class HammerItem extends PickaxeItem {
+public class HammerItem extends PickaxeItem{
     public HammerItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
         super(tier, attackDamageIn, attackSpeedIn, builder);
     }
